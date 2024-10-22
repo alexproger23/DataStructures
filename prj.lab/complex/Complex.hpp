f49176d768faaacb36f87edca22574ecddff4ad0
alexproger23 <sashakuly.2006@gmail.com>
2024-10-22 17:21:18 +0300

    Add files via upload

---
+++ prj.lab/complex/Complex.hpp
@@ -0,0 +1,37 @@
+﻿#ifndef COMPLEX_HPP
+#define COMPLEX_HPP
+
+#include <ostream>
+#include <istream>
+
+struct Complex {
+	double re;
+	double im;
+
+	static const char leftBrace{ '{' };
+	static const char separator{ ',' };
+	static const char rightBrace{ '}' };
+
+	Complex() : re(0.0), im(0.0) {}
+	explicit Complex(const double& real);
+	Complex(const double& real, const double& imag);
+
+	Complex& operator+=(const Complex& rhs);
+	Complex& operator+=(const double& rhs);
+	Complex& operator-=(const Complex& rhs);
+	Complex& operator-=(const double& rhs);
+	Complex& operator*=(const Complex& rhs);
+	Complex operator-() const { return Complex(-re, -im); }
+
+	~Complex() {};
+};
+
+std::ostream& operator<<(std::ostream& ostr, const Complex& cmpl);
+std::istream& operator>>(std::istream& ostr, Complex& cmpl);
+
+Complex operator+(const Complex& lhs, const Complex& rhs);
+Complex operator-(const Complex& lhs, const Complex& rhs);
+Complex operator*(const Complex& lhs, const Complex& rhs);
+
+
+#endif // !COMPLEX.HPP
